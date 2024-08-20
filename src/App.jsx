130f023b413fd6411f8fc7d083ecb6c2654e825b
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-08-20 15:28:13 +0530

    all update
    done

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import BodyContent from './Components/BodyContent/BodyContent'
+import Footer from './Components/FooterContent/Footer'
+import HeaderContent from './Components/HeaderContent/HeaderContent'
+
+function App() {
+
+  return (
+    <>
+      <HeaderContent />
+      <BodyContent>
+
+        <div className="row">
+           <div className="left">
+              <img src='2.png' alt='' />
+           </div>
+           <div className='content'>
+              <h1>Welcome to the World of DAOs</h1>
+              <p>Explore the future of decentralized organizations where decisions are made by the community, for the community. DAOs (Decentralized Autonomous Organizations) empower individuals to collaborate and govern collectively, all without the need for central authorities.
+               Discover how DAOs are reshaping industries, fostering innovation, and building global communities...</p>
+           </div>
+
+        </div>
+
+
+
+
+      </BodyContent>
+      <Footer/>
+
+
+    </>
+  )
+}
+
+export default App
